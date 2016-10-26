3de71305801c2a8001dbd4a0e3d485d6e3cfa92c
张哲 <hi@kxrr.us>
2016-10-26 23:25:45 +0800

    调整query结构

--- jxaumaster/data/models.py
+++ jxaumaster/data/models.py
@@ -37,8 +37,7 @@ class Student(MyDocument):
 
     @classmethod
     def query(cls, **query):
-        EXCLUDES = ['id']
-        return cls.objects(**query).exclude(*EXCLUDES)
+
 
     def __str__(self):
         return unicode(self).encode('utf-8')
--- jxaumaster/handlers/query.py
+++ jxaumaster/handlers/query.py
@@ -7,14 +7,13 @@ class StudentQueryHandler(BaseHandler):
     """
     TODO: __contains...支持, 添加动态参数(phone_url)
     """
-    AVAILABLE_FIELDS = ('sid', 'name')
 
     def get(self, *args, **kwargs):
-        query = self.get_query_args()
+        q = self.get_argument('q', strip=True, default='').split(',')
         field_filter = self.get_argument('only', '', strip=True).split(',')
         photo = self.get_argument('photo', False)
 
-        students = self.query_students(query, field_filter)
+        students = StudentQuery(Builder(q)).query(field_filter)
 
         if photo:
             self.attach_phone_link(students)
@@ -27,22 +26,68 @@ class StudentQueryHandler(BaseHandler):
             student['phone_link'] = 'http://prefix/{}'.format(student['sid'])
         return students
 
-    def query_students(self, query, field_filter):
+
+class Builder(object):
+    """
+    配置Document.objects所需的参数
+    """
+    AVAILABLE_FIELDS = ('sid', 'name')
+    CUSTOM_FIELDS = ('hometown',)
+
+    def __init__(self, query_list):
+        """
+        :type query_list: dict
+        """
+        self.query_kw = dict(map(lambda s: s.split('='), query_list))
+
+    def build(self):
+        query = {}
+        query.update(self.build_custom_kw())
+        query.update(self.build_mongo_kw())
+        return query
+
+    def build_mongo_kw(self):
+        _q = {}
+        for k, v in self.query_kw.copy().items():
+            if k in self.AVAILABLE_FIELDS:
+                _q[k] = v
+                self.query_kw.pop(k, None)  # 这里处理过后续就不需要处理了
+        return _q
+
+    def build_custom_kw(self):
+        """
+        :return:
+        """
+        _q = {}
+        hometown = self.query_kw.pop('hometown', None)
+        if hometown:
+            _q['hometown'] = self.fix_hometown(hometown)
+        return _q
+
+    def fix_hometown(self, ht):
+        return ht
+
+
+class Query(object):
+    """
+    执行查询并做一些后续工作, 比如only
+    """
+
+    def __init__(self, builder):
+        self.builder = builder
+
+
+class StudentQuery(Query):
+    DEFAULT_EXCLUDES = {'id', }
+
+    def query(self, field_filter):
         """
         :rtype: dict
         """
-        students = Student.query(**query)
+        objects_kw = self.builder.build()
+        students = Student.objects(**objects_kw).exclude(*self.DEFAULT_EXCLUDES)
+
         if any(field_filter):
             students = students.only(*field_filter)
 
         return Student.to_python(students)
-
-    def get_query_args(self):
-        query = {}
-        args = self.get_argument('q', strip=True, default='').split(',')
-        for arg in args:
-            arg = arg.strip()
-            k, v = arg.split('=')
-            if k in self.AVAILABLE_FIELDS:
-                query[k] = v
-        return query
